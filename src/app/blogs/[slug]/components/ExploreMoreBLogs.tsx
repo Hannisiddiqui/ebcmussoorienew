ce21957e9e5c9c01e77f8efceb1e4fcc2da5a58b
Personal <sk7627577@gmail.com>
2026-04-01 16:18:13 +0530

    css fix

--- src/app/blogs/[slug]/components/ExploreMoreBLogs.tsx
+++ src/app/blogs/[slug]/components/ExploreMoreBLogs.tsx
@@ -20,22 +20,22 @@ interface Props {
 const ExploreMoreBLogs: React.FC<Props> = ({ title, cta, cards }) => {
   return (
     <SectionWithContainer sectionClassName="bg-primary">
-        <div className="flex items-center justify-between mb-8 md:mb-16">
+        <div className="flex max-md:flex-col gap-6 items-center justify-between mb-8 md:mb-16">
             <SectionHeading title={title} titleColor="white" />
-            <CtaBtn label={cta.title} href={cta.url} type="link" className="rounded-md bg-background-1 text-primary capitalize text-[1.3rem]" />
+            <CtaBtn label={cta.title} href={cta.url} type="link" className="rounded-md bg-background-1 text-primary capitalize text-[1.3rem] w-fit!" />
         </div>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6">
         {cards.map((card, index) => (
           <Link
             href={card.slug}
             key={index}
-            className="relative block w-full aspect-4/3 md:rounded-3xl overflow-hidden  rounded-xl"
+            className="relative block w-full aspect-4/3 rounded-3xl overflow-hidden "
           >
             <Image
               src={card.src}
               alt={card.title}
               fill
-              className="object-cover p-2 bg-background-1 md:rounded-3xl rounded-xl"
+              className="object-cover p-2 bg-background-1 rounded-3xl"
             />
           </Link>
         ))}
