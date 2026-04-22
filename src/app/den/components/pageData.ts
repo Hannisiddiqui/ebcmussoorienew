d584b5b11ff687bf285b4c7364e166e661fcaf51
Personal <sk7627577@gmail.com>
2026-04-22 12:57:38 +0530

    fix issue

--- src/app/den/components/pageData.ts
+++ src/app/den/components/pageData.ts
@@ -9,7 +9,7 @@ const denPageData = {
 
   highlights: {
     image: "/gallery/den/den3.png",
-    title: 'Explore - "The Den"',
+    title: 'Explore - <i>"The Den"</i>',
     description: [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     ],
