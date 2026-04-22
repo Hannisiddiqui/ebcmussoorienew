e776ba7bd949b255e9d8d8b55b4e158a220756db
Personal <sk7627577@gmail.com>
2026-04-22 16:08:48 +0530

    dan page content update

--- src/app/den/components/pageData.ts
+++ src/app/den/components/pageData.ts
@@ -11,7 +11,8 @@ const denPageData = {
     image: "/gallery/den/den3.png",
     title: 'Explore - <i>"The Den"</i>',
     description: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "George’s Den is our signature indoor lounge, strategically designed to deliver a seamless blend of relaxation, celebrations, and business in a premium, high-conversion setting. With elegant interiors, ambient lighting, and plush seating, it creates an instantly appealing environment for guests looking to unwind, host, or engage.",
+      "The lounge features a curated menu of mocktails, cocktails, and speciality coffee, making it a high-demand space for everything from casual evenings to curated social experiences. Whether it’s a relaxed coffee meet, a lively celebration, or an exclusive cocktail evening, George’s Den enhances every occasion with a refined touch.",
     ],
   },
 
@@ -19,15 +20,27 @@ const denPageData = {
     cards: [
       {
         image: "/gallery/den/den4.png",
-        text: "Dedicated, soundproof Den just for inhouse guests.",
+        text: "Fully optimized venue for corporate meetings, offsites, and private events with a professional yet comfortable setup",
       },
       {
         image: "/gallery/den/den5.png",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        text: "Equipped with a 71-inch TV, ideal for presentations, strategy sessions, and team discussions",
       },
       {
         image: "/gallery/den/den6.png",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        text: "Direct access to an expansive front lawn enabling seamless indoor-outdoor event integration",
+      },
+      {
+        image: "/gallery/den/den7.png",
+        text: "Outdoor capacity of up to 200 guests, suitable for large-scale celebrations and corporate gatherings",
+      },
+      {
+        image: "/gallery/den/den8.png",
+        text: "Multi-functional design catering to both leisure and corporate segments, including destination and experiential events",
+      },
+      {
+        image: "/gallery/den/den9.png",
+        text: " Premium positioning with strong revenue potential, enhancing overall guest experience and property value",
       },
     ],
   },
